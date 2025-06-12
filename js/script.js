1690337acb5fb00800960edeb06c19eec06fe1c4
Davi do Couto <162334727+Davicouto530@users.noreply.github.com>
2025-06-12 18:39:46 -0300

    Área do sobre finalizada na página inicial.

--- js/script.js
+++ js/script.js
@@ -18,10 +18,5 @@ const swiper = new Swiper('.swiper', {
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true
-
     }
 });
