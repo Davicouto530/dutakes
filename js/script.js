1203c015ede2358a297adc4c58a0b5c4205d8387
Davi do Couto <162334727+Davicouto530@users.noreply.github.com>
2025-07-12 16:18:38 -0300

    Colocando o scroll animation em algumas páginas do site.

--- js/script.js
+++ js/script.js
@@ -39,3 +39,17 @@ const dataAno = data.getFullYear();
 
 pRodape.innerHTML += ` ${dataAno} fotografias de dutakes`;
 
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry);
+        if(entry.isIntersecting) {
+            entry.target.classList.add('show');
+            observer.unobserve(entry.target);
+        }else {
+            entry.target.classList.remove('show');
+        }
+    });
+})
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
